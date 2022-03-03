af1e2d4e7a4035effa1dfb68e95918e8c60041f0
vanhoa10 <97646929+vanhoa10@users.noreply.github.com>
2022-03-03 17:20:29 +0700

    Update add.php

--- app/Views/empl/add.php
+++ app/Views/empl/add.php
@@ -2,69 +2,85 @@
 <html lang="en">
 
 <head>
-  <title>home</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-  <style>
-    table,
-    th,
-    td {
-      border: 1px solid black;
-      border-collapse: collapse;
-    }
-  </style>
+    <title>home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
 </head>
 
 <body>
-  <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-    <ul class="nav nav-tabs">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="/">
-          <font color="blue"><b>Danh sách</b></font>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="create">
-          <font color="blue"><b>Thêm nhân viên</b> </font>
-        </a>
-    </ul>
-  </nav>
-  <br>
-  <h>
-    <center><b>
-        <font color="orange" size="+2" face="Verdana"> QUẢN LÍ NHÂN VIÊN </font>
-      </b></center>
-  </h></br>
-  <table cellspacing="0" cellpadding="7" align="center">
-    <tr align="center">
-      <th width="100">ID</th>
-      <th width="200">Tên</th>
-      <th width="100">Ngày sinh</th>
-      <th width="150">Số điện thoại</th>
-      <th width="120">Chức danh</th>
-      <th width="120"></th>
-      <th width="120"></th>
-    </tr>
-    <tbody>
-      <?php
-      foreach ($if_employee as $if_employee) { ?>
-        <tr align="center">
-          <td><?= $if_employee->id ?></td>
-          <td><?= $if_employee->name ?></td>
-          <td><?= $if_employee->birth ?></td>
-          <td><?= $if_employee->phonenumber ?></td>
-          <td><?= $if_employee->position ?></td>
-          <td><a href="<?=base_url('show/'.$if_employee->id)?>" class="btn btn-outline-info" tabindex="-1" role="button" aria-disabled="true">chi tiết</a></td>
-          <td>
-            <a href="<?=base_url('delete/'.$if_employee->id)?>" class="btn btn-outline-dark" tabindex="-1" role="button" aria-disabled="true">xóa</a></td>
-          </tr>
-      <?php } ?>
-    </tbody>
-  </table>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">
+                    <font color="blue"><b>Danh sách</b></font>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="create">
+                    <font color="blue"><b>Thêm nhân viên</b> </font>
+                </a>
+        </ul>
+    </nav>
+    <br>
+    <h>
+        <center><b>
+                <font color="orange" size="+2" face="Verdana"> THÊM NHÂN VIÊN MỚI </font>
+            </b></center>
+    </h></br>
+    <form method="POST" action="<? base_url('save')?>">
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">ID</span>
+        <input type="text" class="form-control" placeholder="nhập id" aria-label="id" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Tên</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="name" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="birth" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="hometown" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="phonenumber" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="position" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="department" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="identification_card" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="issued_on" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="placed_on" aria-describedby="basic-addon1">
+    </div>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 
 </html>
